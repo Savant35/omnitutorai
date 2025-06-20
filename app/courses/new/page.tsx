9e13b06df052e3bc55690dfb6a66b0fb272010f3
Ali Arthur <aliarthur35@gmail.com>
2025-06-20 15:09:28 -0400

    made form to create agent

--- app/courses/new/page.tsx
+++
@@ -1,8 +0,0 @@
-
-const page = () => {
-  return (
-    <div>new companion</div>
-  )
-}
-
-export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button'
-import CompanionCard from '@/components/ui/CompanionCard'
-import CompanionList from '@/components/ui/CompanionList'
-import CTA from '@/components/ui/CTA'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { recentSessions } from '@/constants'
 import React from 'react'
 
@@ -36,11 +36,11 @@ const Page = () => {
         />
       </section>
 
-      <section className='home-section"'>
+      <section className="home-section">
         <CompanionList 
         title="Recent completed Sessions"
         companions={recentSessions}
-        classNames="w-2/3 max-lg:w-full"
+         classNames="w-2/3 max-lg:w-full"
         />
         <CTA />
       </section>
